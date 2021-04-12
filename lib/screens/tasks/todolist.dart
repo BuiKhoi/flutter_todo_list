9ed9cb0b7afdbcebe539134b6a647f9ae2efd47a
phuoctam <phuoctam3009@gmail.com>
2021-04-12 15:39:26 +0700

    Add button add

--- lib/screens/tasks/todolist.dart
+++ lib/screens/tasks/todolist.dart
@@ -4,36 +4,60 @@ import 'package:todolist/models/todoList.dart';
 import 'package:todolist/screens/tasks/task.dart';
 import 'package:provider/provider.dart';
 
-class TodoListWidget extends StatelessWidget {
-  final TextEditingController _controller = TextEditingController();
+//widget class
 
+class TodoListWidget extends StatelessWidget {
+  final _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-      Expanded(child: Consumer<TodoListModel>(builder: (context, tasks, child) {
-        return ListView(
-          children: tasks.tasks.map((TaskModel task) {
-            return ChangeNotifierProvider.value(
-                value: task, child: TaskWidget());
-          }).toList(),
-        );
-      })),
-      Consumer<TodoListModel>(
-        builder: (contexst, tasks, child) {
-          return TextField(
-            controller: _controller,
-            decoration: InputDecoration(
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.teal)),
-                labelText: 'new task'),
-            onSubmitted: (newTask) {
-              tasks.addTaks(TaskModel(text: newTask)); // create new instance of task changeNotifier model
-              _controller.clear(); // clear the text input after adding taks
-              tasks.saveTasksToSharedPrefs();
-            },
+      Expanded(
+        child: Consumer<TodoListModel>(builder: (context, todoList, child) {
+          return ListView(
+            children: todoList.tasks.map((TaskModel task) {
+              return ChangeNotifierProvider.value(
+                  value: task, child: TaskWidget());
+            }).toList(),
           );
-        },
-      )
+        }),
+      ),
+      Consumer<TodoListModel>(builder: (contexst, todoList, child) {
+        return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                  child: Column(children: <Widget>[
+                Container(
+                    child: TextField(
+                        controller: _controller,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.teal)),
+                            labelText: 'new task'),
+                        onSubmitted: (newTask) {
+                          todoList.addTaks(TaskModel(
+                              text:
+                                  newTask)); // create new instance of task changeNotifier model
+                          _controller
+                              .clear(); // clear the text input after adding taks
+                          todoList.saveTasksToSharedPrefs();
+                        }))
+              ])),
+              Expanded(
+                  child: Column(children: <Widget>[
+                Container(
+                    child: FloatingActionButton(
+                        onPressed: () {
+                          todoList.addTaks(TaskModel(text: _controller.text));
+                          _controller.clear();
+                          todoList.saveTasksToSharedPrefs();
+                        },
+                        tooltip: 'Add Todo',
+                        child: const Icon(Icons.add)))
+              ]))
+            ]);
+      }),
     ]);
   }
 }
